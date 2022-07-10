0387661b9c7906d9ad9dabc64b6c066cc69c7ce3
Gabriel Teixeira <gabriel.404e@gmail.com>
2022-07-10 00:10:02 -0300

    upgrade to react 18

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,15 +8,23 @@ import "@fontsource/roboto";
 import'@mui/icons-material/AccessAlarm';
 import'@mui/icons-material/ThreeDRotation';
 
-
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+  <BrowserRouter>
+    <App tab="home" />
+  </BrowserRouter>
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -4,8 +4,7 @@ import Nav from "../components/nav/nav.js";
 import {Avatar} from "@mui/material";
 import { useSpring, animated } from 'react-spring'
 
-
-
+// Problema com o SVG: Ele é grande demais e esta ofuscando a nav 
 const Home = (props) => {
     const POINTS = '200,10 250,190 160,210'
     function SVG() {
@@ -22,7 +21,7 @@ const Home = (props) => {
       
         return (
           <animated.svg
-            style={{ position:"absolute", width: 1200, height: 1000 }}
+            style={{overflow:'visible',position:"absolute", width: 1200, height: 1000 }}
             
             //strokeWidth="2"
             //fill="white"
@@ -33,9 +32,12 @@ const Home = (props) => {
             //strokeDashoffset={x.to(x => (1 - x) * 156)}
             >
             <path d="M 0 600 q 600 100 1200 0" style={{stroke:'blue',fill:"#222",strokeWidth:5}} />
-            <g stroke="black" stroke-width="3" fill="black">
-                <circle id="start" cx="0" cy="600" r="3" />
-                <circle id="end" cx="1200" cy="600" r="3" />
+            <g stroke="black" strokeWidth="3" fill="red">
+                <circle id="P-0-S" cx="0" cy="600" r="3" />
+                <circle id="P-1" cx="300" cy="637" r="3" />
+                <circle id="P-2-M" cx="600" cy="650" r="3" />
+                <circle id="P-3" cx="900" cy="637" r="3" />
+                <circle id="P-4-E" cx="1200" cy="600" r="3" />
             </g>
           </animated.svg>
         )
