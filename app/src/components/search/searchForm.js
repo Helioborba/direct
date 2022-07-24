a231510174a88b46799a534a012677b712d95705
Gabriel Teixeira <gabriel.404e@gmail.com>
2022-07-23 23:35:06 -0300

    decouple the search into Search form/ search result componenets

--- app/src/components/search/searchForm.js
+++ app/src/components/search/searchForm.js
@@ -1,26 +1,21 @@
 import {Typography, Grid} from "@mui/material";
-import { FormControl, TextField, IconButton } from "@mui/material";
+import { FormControl, TextField } from "@mui/material";
 import Button from "../UI/buttons/button";
-import CreateNewFolderIcon from '@mui/icons-material/CreateNewFolder';
-import SendIcon from '@mui/icons-material/Send';
-import Input from "./input.js";
+import SearchIcon from '@mui/icons-material/Search';
 
 const SearchForm = (props) => {
     return (
         <FormControl component="form" onSubmit={props.sendMessage} sx={{width:"100%"}}>
-            <Grid container direction='row'>
-                <Grid item xs={1} display='flex' justifyContent='center' alignItems='center'>   
-                    <IconButton component='button' onClick={() => props.handleClick()} sx={{display:'flex', justifyContent:'center', alignItems:'center', p:0, color:"#fff"}}>
-                        <CreateNewFolderIcon sx={{ width:60, height:60}}></CreateNewFolderIcon>
-                        <Input type="file" onChange={(e) => props.handleFileChange(e)} ref={props.fileInput} id="anex-chat-file" style={{display:'none'}}/>
-                    </IconButton>                               
+            <Grid container direction='column' sx={{p:20}}>
+                <Grid item>
+                    <Typography>Type the name of the person or group ID to search</Typography>
                 </Grid>
-                <Grid item xs={9}>
+                <Grid item>
                     <TextField
                         id="input-field-chat"
                         inputRef={props.chatField}
                         fullWidth
-                        placeholder={"Write your message"}
+                        placeholder={"Person name or Group ID"}
                         InputLabelProps={{
                             sx: { color: '#fff'}
                         }}
@@ -44,14 +39,14 @@ const SearchForm = (props) => {
                         }}
                     />
                 </Grid>
-                <Grid item xs={2} sx={{display:'flex', justifyContent:'center', alignItems:'center', p:0}}>
+                <Grid item sx={{display:'flex', justifyContent:'center', alignItems:'center', pt: 4}}>
                     <Button type={'submit'}>
                         <Grid container sx={{display:'flex', justifyContent:'center', alignItems:'center', p:2}}>
                             <Grid item xs={10} sx={{display:'flex', justifyContent:'space-between', alignItems:'center'}}>
-                                <Typography>Send</Typography>
+                                <Typography>Search</Typography>
                             </Grid>
                             <Grid item xs={2} sx={{display:'flex', justifyContent:'center', alignItems:'center',}}>
-                                <SendIcon/>
+                                <SearchIcon/>
                             </Grid>
                         </Grid>
                     </Button>
--- app/src/pages/Search.js
+++ app/src/pages/Search.js
@@ -1,11 +1,20 @@
-// import { useContext } from "react";
+import { useState } from "react";
 import {Avatar, Box, Grid, Typography} from "@mui/material";
 import Nav from "../components/nav/nav.js";
 import ModelPagination from "../components/pagination/pagination.js";
 import Button from "../components/UI/buttons/button.js";
 import icon from "../others/images/icon.png";
 import Image from "../components/UI/image/image.js";
+import SearchForm from "../components/search/searchForm.js";
+import SearchResult from "../components/search/searchResult.js";
 const Search = (props) => {
+    const [search, setSearch] = useState(true)
+
+    // Will run after searching for a person, to render the other comp
+    function setSearchHandler() {
+        setSearch(!search);
+    }
+    
     // const ctxNations = useContext(NationsContext);
 
     // for (const node of ctxNations.canvasNodes) {
@@ -32,79 +41,13 @@ const Search = (props) => {
         
 
     // }
-    function render() {
-        return (<Typography>Nothing Here</Typography>)
-    }
 
     return(
         <Box sx={{width:"100vw", minHeight:"100vh", backgroundColor:"#333"}}>
             <Nav></Nav>
             <Box sx={{p:{xs:"5rem 0 5rem 0", lg:2}, display:'flex', justifyContent:'space-between', alignItems:'center'}}>
                 <Grid container direction="column"  sx={{p:3, backgroundColor:"#222", display:"flex", justifyContent:"space-between", alignItems:"center",  height:"88vh", borderRadius:{xs:0, lg:3} }}>
-                    <Grid item xs={1} sx={{position:'relative', width:'100%', display:"flex", justifyContent:"end", alignItems:"center",}}>
-                        {/* Theres some padding left here to fix the position */}
-                        <Typography sx={{flex:1, pl:5}}>Results</Typography>          
-                        <Button>Go back</Button>
-                    </Grid>
-                    <Grid container direction="column" item xs={10} sx={{display:"flex", alignItems:"center"}}>
-                        <Grid container item xs={4} sx={{display:"flex", justifyContent:"space-between", alignItems:"center"}}>
-                            <Grid item xs={3} sx={{display:"flex", alignItems:"center", flexDirection:"column"}}> 
-                                <Image img={icon}></Image>
-                                <Typography pt={2}>ASARERAWERERARER</Typography>
-                            </Grid>
-                            <Grid item xs={3} sx={{display:"flex", alignItems:"center", flexDirection:"column"}}> 
-                                <Image img={icon}></Image>
-                                <Typography pt={2}>ASARERAWERERARER</Typography>
-                            </Grid>
-                            <Grid item xs={3} sx={{display:"flex", alignItems:"center", flexDirection:"column"}}> 
-                                <Image img={icon}></Image>
-                                <Typography pt={2}>ASARERAWERERARER</Typography>
-                            </Grid>
-                            <Grid item xs={3} sx={{display:"flex", alignItems:"center", flexDirection:"column"}}> 
-                                <Image img={icon}></Image>
-                                <Typography pt={2}>ASARERAWERERARER</Typography>
-                            </Grid>
-                        </Grid>
-                        <Grid container item xs={4} sx={{display:"flex", justifyContent:"space-between", alignItems:"center"}}>
-                            <Grid item xs={3} sx={{display:"flex", alignItems:"center", flexDirection:"column"}}> 
-                                <Image img={icon}></Image>
-                                <Typography pt={2}>ASARERAWERERARER</Typography>
-                            </Grid>
-                            <Grid item xs={3} sx={{display:"flex", alignItems:"center", flexDirection:"column"}}> 
-                                <Image img={icon}></Image>
-                                <Typography pt={2}>ASARERAWERERARER</Typography>
-                            </Grid>
-                            <Grid item xs={3} sx={{display:"flex", alignItems:"center", flexDirection:"column"}}> 
-                                <Image img={icon}></Image>
-                                <Typography pt={2}>ASARERAWERERARER</Typography>
-                            </Grid>
-                            <Grid item xs={3} sx={{display:"flex", alignItems:"center", flexDirection:"column"}}> 
-                                <Image img={icon}></Image>
-                                <Typography pt={2}>ASARERAWERERARER</Typography>
-                            </Grid>   
-                        </Grid>
-                        <Grid container item xs={4} sx={{display:"flex", justifyContent:"space-between", alignItems:"center"}}>
-                            <Grid item xs={3} sx={{display:"flex", alignItems:"center", flexDirection:"column"}}> 
-                                <Image img={icon}></Image>
-                                <Typography pt={2}>ASARERAWERERARER</Typography>
-                            </Grid>
-                            <Grid item xs={3} sx={{display:"flex", alignItems:"center", flexDirection:"column"}}> 
-                                <Image img={icon}></Image>
-                                <Typography pt={2}>ASARERAWERERARER</Typography>
-                            </Grid>
-                            <Grid item xs={3} sx={{display:"flex", alignItems:"center", flexDirection:"column"}}> 
-                                <Image img={icon}></Image>
-                                <Typography pt={2}>ASARERAWERERARER</Typography>
-                            </Grid>
-                            <Grid item xs={3} sx={{display:"flex", alignItems:"center", flexDirection:"column"}}> 
-                                <Image img={icon}></Image>
-                                <Typography pt={2}>ASARERAWERERARER</Typography>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                    <Grid item xs={1} sx={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-                        <ModelPagination></ModelPagination>
-                    </Grid>
+                    {search ? <SearchForm></SearchForm> : <SearchResult></SearchResult>}
                 </Grid>
             </Box>
         </Box>
