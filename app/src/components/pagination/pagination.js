0a0d904b3f393d65efa2ca710926080ebe44c29a
Gabriel Teixeira <gabriel.404e@gmail.com>
2022-07-26 01:54:01 -0300

    add render for the search results

--- app/src/components/pagination/pagination.js
+++ app/src/components/pagination/pagination.js
@@ -2,10 +2,18 @@ import * as React from 'react';
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
 
-export default function ModelPagination() {
+const ModelPagination = (props) => {
   return (
     <Stack spacing={2}>
-      <Pagination count={10} color="primary" sx={{ "& .MuiPaginationItem-root": {color: "#fff"}}}/>
+      <Pagination
+        count={props.numberOfPages} 
+        color="primary" 
+        sx={{ "& .MuiPaginationItem-root": {color: "#fff"}}}
+        page={props.currentPage}
+        onChange={props.currentPageHandler}
+      />
     </Stack>
   );
 }
+
+export default ModelPagination;
--- app/src/components/search/searchResult.js
+++ app/src/components/search/searchResult.js
@@ -3,15 +3,55 @@ import ModelPagination from "../pagination/pagination.js";
 import Button from "../UI/buttons/button.js";
 import icon from "../../others/images/icon.png";
 import Image from "../UI/image/image.js";
-import React from "react";
+import React, {useState} from "react";
 import SearchInnerGrid from "./searchInnerGrid.js";
 const SearchResult = (props) => {
+    const [currentPage, setCurrentPage] = useState(1); // Pagination current page
+    let numberOfPages = 3; // Holds the amount of necessary pages needed to render the other grids
+
+    // receives both the event and value
+    const currentPageHandler = (e, value) => {
+        setCurrentPage(value);
+    }
+
     function render() {
-        // 
-        const componentInnerGridArray = props.result.map((data) => {
-            return <SearchInnerGrid>{data.username}</SearchInnerGrid>
-        })
-        return componentInnerGridArray
+        // Create all of the inner grids 
+        const componentGroup = []; // a array containing the inner grids in a collection of 4
+        const subGroup = []; // array containing the jsx inner grids
+
+        for (const [key, value] of Object.entries(props.result)) {
+            let updatedKey = Number(key) + 1;
+            
+            subGroup.push(<SearchInnerGrid>{value.username}</SearchInnerGrid>); // add the current value to the sub group
+        
+            // add the subgroup
+            if (updatedKey % 4 === 0) {
+                componentGroup.push([...subGroup]); // add the whole jsx group to the componentGroup
+                subGroup.length = 0; // flush the array
+                console.log(componentGroup)
+            }
+            
+            // Add the rest of the data to the group array when there's no more groups to form
+            if (props.result.length === updatedKey) {
+                componentGroup.push(subGroup);
+            }
+        }
+        // const componentInnerGridArray = props.result.map((data, index) => {
+        //     // Used to set if the component can be wrapped later on inside the outer grid
+        //     if (index % 4 === 0) {
+        //         keyIndex += 1;
+        //     }
+            
+        //     return { grid: [keyIndex], component: <SearchInnerGrid>{data.username}</SearchInnerGrid>}
+        // })
+
+        // Wrap every 4 innerGrids to the OuterGrid to complete the row
+        // const assembledComponents = componentInnerGridArray.map( (component) => {
+            
+        // })
+
+
+        // return componentInnerGridArray
     }
     return(
         <React.Fragment>
@@ -62,7 +102,7 @@ const SearchResult = (props) => {
                 </Grid> */}
             </Grid>
             <Grid item xs={1} sx={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-                <ModelPagination></ModelPagination>
+                <ModelPagination currentPage={currentPage} currentPageHandler={currentPageHandler} numberOfPages={numberOfPages}></ModelPagination>
             </Grid>
         </React.Fragment>
     )
